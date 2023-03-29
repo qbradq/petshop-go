570f68fb2eed92f7dae69d2b3b9c9b6c2b311c7e
Norman B. Lancaster <qbradq@gmail.com>
2023-03-29 00:07:20 -0500

    Static page basic structure

--- cmd/staticd/main.go
+++ cmd/staticd/main.go
@@ -1,16 +1,40 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path"
 
 	"github.com/qbradq/petshop-go/data"
 )
 
+// tpl is the pre-parsed template file system
+var tpl *template.Template
+
+// templateHandler renders HTML templates
+func templateHandler(response http.ResponseWriter, request *http.Request) {
+	rn := path.Base(request.RequestURI)
+	tn := "index.html.tpl"
+	if rn != "/" && rn != "." {
+		tn = rn + ".tpl"
+	}
+	tpl.ExecuteTemplate(response, tn, nil)
+}
+
 // staticd is the static asset and template service for Pet Shop.
 func main() {
+	var err error
+	// Pre-load templates
+	tpl, err = template.ParseFS(data.TemplateFS, "templates/*")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Configure http server
 	hfs := http.FileServer(http.FS(data.StaticFS))
-	http.Handle("/", hfs)
+	http.Handle("/static/", hfs)
+	http.HandleFunc("/", templateHandler)
+	// Start the server
 	if err := http.ListenAndServe("0.0.0.0:8080", nil); err != nil {
 		log.Println(err)
 	}
--- data/data.go
+++ data/data.go
@@ -6,3 +6,8 @@ import "embed"
 //
 //go:embed static
 var StaticFS embed.FS
+
+// TemplateFS is the template file system for the web server.
+//
+//go:embed templates
+var TemplateFS embed.FS
