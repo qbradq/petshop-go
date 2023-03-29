47dfadee149d8b99a6187c4526c6819db039c923
Norman B. Lancaster <qbradq@gmail.com>
2023-03-29 05:08:21 -0500

    Added list form and handler skeleton

--- cmd/staticd/main.go
+++ cmd/staticd/main.go
@@ -12,14 +12,27 @@ import (
 // tpl is the pre-parsed template file system
 var tpl *template.Template
 
-// templateHandler renders HTML templates
-func templateHandler(response http.ResponseWriter, request *http.Request) {
-	rn := path.Base(request.RequestURI)
+// templateHandler renders HTML templates.
+func templateHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	rn := path.Base(r.RequestURI)
 	tn := "index.html.tpl"
 	if rn != "/" && rn != "." {
 		tn = rn + ".tpl"
 	}
-	tpl.ExecuteTemplate(response, tn, nil)
+	tpl.ExecuteTemplate(w, tn, nil)
+}
+
+// listHandler handles listing new pets for adoption.
+func listHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.Redirect(w, r, "/api/view/1234", http.StatusFound)
 }
 
 // staticd is the static asset and template service for Pet Shop.
@@ -33,6 +46,7 @@ func main() {
 	// Configure http server
 	hfs := http.FileServer(http.FS(data.StaticFS))
 	http.Handle("/static/", hfs)
+	http.HandleFunc("/api/list", listHandler)
 	http.HandleFunc("/", templateHandler)
 	// Start the server
 	if err := http.ListenAndServe("0.0.0.0:8080", nil); err != nil {
--- data/templates/list.html.tpl
+++ data/templates/list.html.tpl
@@ -2,4 +2,36 @@
 
 <h1 class="title">List a Pet</h1>
 
+<style>
+img#picture-preview {
+    width: 100%;
+    display: none;
+}
+</style>
+
+<script>
+    function preview(event) {
+        if(event.target.files.length > 0){
+            var s = URL.createObjectURL(event.target.files[0]);
+            var p = document.getElementById("picture-preview");
+            p.src = s;
+            p.style.display = "block";
+        }
+    }
+</script>
+
+<form action="/api/list" method="post">
+    <img id="picture-preview" />
+    <label for="picture">Upload Picture</label>
+    <input type="file" name="picture" accept="image/*" onchange="preview(event);" />
+    <br />
+    <label for="name">Name</label>
+    <input type="text" name="name" />
+    <br />
+    <label for="description">Description</label>
+    <textarea name="description" rows="10" cols="80"></textarea>
+    <br />
+    <input type="submit" value="Submit" />
+</form>
+
 {{ template "footer.html.tpl" }}
